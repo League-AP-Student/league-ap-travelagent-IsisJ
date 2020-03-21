427a119616905778feb309ccfdf36dc54afd7b7c
IsisJ <isisofalexandria@gmail.com>
2020-03-20 21:53:06 -0700

    Finished--needs reviewing

--- src/Trip.java
+++ src/Trip.java
@@ -5,11 +5,37 @@ public class Trip {
 
 	public int getDuration() {
 		int duration = 0;
+		Time departure;
+		Time arrival;
+		if(flights.size() < 0) {
+			return 0;
+		}
+		for(Flight f : flights) {
+			departure = f.getDeparturetime();
+			arrival = f.getArrivalTime();
+			duration = duration + departure.minutesUntil(arrival);
+			departure = null;
+			arrival = null;	
+		}
 		return duration;
 	}
 
 	public int getShortestLayover() {
-		int shortest = -1;
+		int shortest;
+		int comparable;
+		
+		if(flights.size()<2) {
+			return -1;
+		}
+		
+		shortest = flights.get(0).getArrivalTime().minutesUntil(flights.get(1).getDeparturetime());
+		
+		for(int i=1; i<flights.size()-1; i++) {
+			comparable = flights.get(i).getArrivalTime().minutesUntil(flights.get(i+1).getDeparturetime());
+			if(shortest > comparable) {
+				shortest = comparable;
+			}
+		}
 		return shortest;
 	}
 }
